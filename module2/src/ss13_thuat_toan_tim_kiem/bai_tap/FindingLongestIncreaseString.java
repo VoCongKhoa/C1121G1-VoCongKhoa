d87be6f8d866b8de2690aad576f332eef8b8f4db
VCKhoa <youandme8668@gmail.com>
2022-01-18 15:57:06 +0700

    up bai tap

--- module2/src/ss13_thuat_toan_tim_kiem/bai_tap/FindingLongestIncreaseString.java
+++ module2/src/ss13_thuat_toan_tim_kiem/bai_tap/FindingLongestIncreaseString.java
@@ -6,26 +6,24 @@ import java.util.List;
 import java.util.Map;
 
 public class FindingLongestIncreaseString {
+
     public static void main(String[] args) {
-        String str = "Jqtoabcde";
+        String str = "Welcome";
         String strTemp = "";
         String strResult = "";
-        Map<Integer,String> stringMap = new HashMap<>();
 
         for (int i = 0; i < str.length(); i++) {
             strTemp += str.charAt(i);
             for (int j = i; j < str.length()-1; j++) {
-                strTemp += str.charAt(j);
-                if (str.charAt(j) <= str.charAt(j+1)) {
+                if (str.charAt(j) < str.charAt(j+1)) {
                     strTemp = strTemp + str.charAt(j+1);
                 }
             }
-            stringMap.put(strTemp.length(),strTemp);
-//            if (strTemp.length() > strResult.length()) {
-//                strResult = strTemp;
-//            }
+            if (strTemp.length() > strResult.length()) {
+                strResult = strTemp;
+            }
             strTemp = "";
         }
-        System.out.println(stringMap);
+        System.out.println(strResult);
     }
 }
